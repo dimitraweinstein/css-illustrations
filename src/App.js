f8cd1d411c3d755636a39db34743cbe408a65af9
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-12-10 17:55:29 -0800

    refactored lamp illustration into its own component and created div for new illustration in app.js, added illustrations folder to file structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+import Lamp from './illustrations/Lamp';
 import './App.css';
 
 function App() {
   return (
-    <div className="lamp">
-      <div className="shade"></div>
-      <div className="leg"></div>
-      <div className="foot"></div>
-    </div>
+    <>
+      <Lamp />
+      <div className="shelf">
+      </div>
+    </>
   );
 }
 
---
+++ src/illustrations/Lamp.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Lamp = () => {
+  return (
+    <div>
+      <div className="lamp">
+        <div className="shade"></div>
+        <div className="leg"></div>
+        <div className="foot"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Lamp;
